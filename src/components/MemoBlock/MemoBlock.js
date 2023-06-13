a994ccffb5212613c8731a813941594f08485a33
fnkbsk <igoico10@gmail.com>
2023-06-13 18:19:09 -0300

    first-commit

--- src/components/MemoBlock/MemoBlock.js
+++ src/components/MemoBlock/MemoBlock.js
@@ -1,1 +1,15 @@
 import './MemoBoard.css';
+
+const MemoBlock = ({memoBlock}) => {
+    <div className='memo-block'>
+        <div className={`memo-block-inner ${memoBlock.flipped && 'memo-block-flipped'}`}>
+            <div className='memo-block-front'>
+            </div>
+            <div className='memo-block-back'>
+                {memoBlock.emoji}
+            </div>
+        </div>
+    </div>
+}
+
+export default MemoBlock;
