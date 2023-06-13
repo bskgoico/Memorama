2fd01c98921d5c00017071988bb320f6f4adad56
fnkbsk <igoico10@gmail.com>
2023-06-13 18:13:28 -0300

    boardComponent

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,1 +1,14 @@
 import './Board.css';
+import MemoBlock from '../MemoBlock/MemoBlock';
+
+const Board = ({memoBlocks}) => {
+    return (
+        <main className='board'>
+            {memoBlocks.map( (memoBlock, i) => {
+                return <MemoBlock key={`${i}_${memoBlock.emoji}`} memoBlock={memoBlock} />
+            })}
+        </main>
+    )
+}
+
+export default Board;
